cc6e277bf9ae5f7b0e28be7e7fef59dd14122383
ZihanWangRepo <wangzihan1989@gmail.com>
2021-12-01 11:59:56 -0800

    Submitted restaurants and restaurant/:id apis

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const axios = require('axios');
+
 
 // Body parser
 const bodyParser = require('body-parser');
@@ -22,4 +27,62 @@ app.get("/hello", (req, res) => {
     res.send("Hello World!");
 })
 
-app.listen(8080);
+app.get("/api/restaurants", (req, res) => {
+  axios.get("http://api.yelp.com/v3/businesses/search", {
+    headers: {
+      "Authorization": `Bearer ${process.env.YELP_API_KEY}`
+    },
+    params : {
+      "location": "seattle, WA"
+    }
+  }).then(businesses => {
+    res.json(businesses.data.businesses[0])
+  })
+})
+
+app.get("/api/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+
+  axios.get(`http://api.yelp.com/v3/businesses/${id}`, {
+    headers: {
+      "Authorization": `Bearer ${process.env.YELP_API_KEY}`
+    }
+    
+  }).then(businesses => {
+    res.json(businesses.data)
+  })
+})
+
+
+// const yelp = require('yelp-fusion');
+// const apiKey = process.env.YELP_API_KEY;
+
+// const searchRequest = {
+//   // term:'Four Barrel Coffee',
+//   location: 'seattle, wa'
+// };
+
+// const client = yelp.client(apiKey);
+// client.search(searchRequest).then(response => 
+//   { businesses = response.jsonBody.businesses;
+//     console.log(businesses);
+//     const getRestaurants = (req, res) => res.json(businesses);
+    
+//     const getRestaurntById = (req, res) => { 
+//       const id = req.params.id;
+//       const restaurant = businesses.filter(business => business.id === id);
+//       res.json(restaurant);
+//     }
+
+//     app.get("/api/restaurants", getRestaurants);
+//     app.get("/api/restaurants/:id", getRestaurntById);
+//   // const prettyJson = JSON.stringify(firstResult, null, 4);
+// }).catch(e => {
+//   console.log(e);
+// });
+
+
+
+
+app.listen(process.env.PORT || 8000);
