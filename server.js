d342087378aef212a91fc2fd3491902b0c9460d3
Fangying Li <li.fangy@northeastern.edu>
2021-12-05 08:22:43 -0500

    slightly change

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 const CONSTANTS = require('./CONSTS');
 const session = require('express-session');
 
-
 const axios = require('axios');
+
+/************************************ Configure CORS************************************/
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", CONSTANTS.CLIENT_URL);
+    res.header("Access-Control-Allow-Headers",
+               "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+               "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+
+/************************************ Configure CORS************************************/
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 // swagger
 
@@ -23,7 +41,7 @@ const options = {
         },
     },
     // List of files to be processes. You can also set globs './routes/*.js'
-    apis: ['./services/auto-completion-service.js','./services/search-service.js'],
+    apis: ['./services/auto-completion-service.js', './services/search-service.js'],
 };
 
 const specs = swaggerJsdoc(options);
@@ -31,39 +49,22 @@ const swaggerUi = require('swagger-ui-express');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 
-// Body parser
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 
-// Configure CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+
 
 app.use(session({
-    secret: 'keyboard cat',
-    cookie: {}
-  }));
+                    secret: 'keyboard cat',
+                    cookie: {}
+                }));
 
 const mongoose = require('mongoose');
 mongoose.connect(CONSTANTS.MONGODB_URL);
 
-
 require('./services/login-service')(app);
-
-
 
 app.get("/hello", (req, res) => {
     res.send("Hello World!");
 })
-
 
 require('./services/restaurant-service')(app);
 require('./services/search-service')(app);
