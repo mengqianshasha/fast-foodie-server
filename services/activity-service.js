5c2ad8424b235ecfe43c50b93c52be10f89f8d34
Qiansha Meng <mengqianshasha@163.com>
2021-12-07 08:36:21 -0800

    update activity service

--- services/activity-service.js
+++ services/activity-service.js
@@ -8,20 +8,23 @@ module.exports = (app) => {
     const getAllCustomerActivities = (req, res) => {
         res.json(defaultActivities);
     }
-    app.get("/api/activities/all", getAllCustomerActivities);
 
 
     // get activities of those who are in the same city with the logged-in user
     const getNearbyCustomerActivities = (req, res) => {
         res.json(nearbyActivities);
     }
-    app.get("/api/activities/nearby", getNearbyCustomerActivities);
 
 
     // get activities of logged-in user's followings
     const getFollowingCustomerActivities = (req, res) => {
         res.json(followingActivities);
     }
+
+
+
+    app.get("/api/activities/all", getAllCustomerActivities);
+    app.get("/api/activities/nearby", getNearbyCustomerActivities);
     app.get("/api/activities/following", getFollowingCustomerActivities);
 
 
