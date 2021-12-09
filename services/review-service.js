42aff72267144c91849260f54c480361c4c231e2
ZihanWangRepo <wangzihan1989@gmail.com>
2021-12-08 19:51:22 -0800

    Updates review apis

--- services/review-service.js
+++ services/review-service.js
@@ -8,14 +8,23 @@ module.exports = (app) => {
   }
 
   const postNewReview = (req, res) => {
-
     const newReview = req.body;
     return reviewDao.createReview(newReview).then(insertedReview => res.send(insertedReview))
 
   }
 
+  const deleteReview = (req, res) => {
+    const reviewId = req.params.reviewId;
+    reviewDao.deleteReview(reviewId).then(status => res.send(status));
+  }
+
+  const saveReview = (req, res) => {
+    reviewDao.updateReview(req.params.reviewId, req.body).then(status => res.send(status))
+  }
+
   app.get('/api/:restaurantId/reviews', findAllReviews);
   app.post('/api/reviews', postNewReview);
-
+  app.delete('/api/reviews/:reviewId', deleteReview);
+  app.put('/api/reviews/:reviewId', saveReview);
 
 };
