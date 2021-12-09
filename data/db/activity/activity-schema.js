b9b2f6173ee5d70c5de11d6e2a6d9625ddb07a5d
Fangying Li <li.fangy@northeastern.edu>
2021-12-08 20:57:05 -0500

    slight update

--- data/db/activity/activity-schema.js
+++ data/db/activity/activity-schema.js
@@ -1,20 +1,10 @@
 const mongoose = require('mongoose');
 
 const activitySchema = mongoose.Schema({
-                                         user: String,
-                                         restaurant: String,
-                                         time_created: String,
-                                         rating: Number,
-                                         text: String,
-                                         img: [{
-                                             url: String,
-                                             text: String
-                                         }],
-                                         replies: [{
-                                             user: String,
-                                             text: String,
-                                             time_created: String
-                                         }]
-                                     }, {collection: 'reviews'});
-
+                                           user: String,
+                                           type: String,
+                                           time_created: String,
+                                           bookmark: String,
+                                           follow: String
+                                       }, {collection: 'reviews'});
 module.exports = activitySchema;
