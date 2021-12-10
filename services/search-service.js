48efe1b77b9c8d326d17a91dfd91e1d30737f51b
Qiansha Meng <mengqianshasha@163.com>
2021-12-09 19:28:37 -0800

    remove swagger

--- services/search-service.js
+++ services/search-service.js
@@ -27,25 +27,6 @@ module.exports = (app) => {
             })
     }
 
-    /**
-     * @swagger
-     * /api/search/{location}:
-     *    get:
-     *      description: This should return search result in terms of location and other optional search params
-     *      parameters:
-     *        - in: path
-     *          name: location
-     *          schema:
-     *            type: string
-     *          required: true
-     *          description: define location of the search
-     *        - in: query
-     *          name: term
-     *          schema:
-     *            type: string
-     *          required: false
-     *          description: define search term
-     */
     app.get("/api/search/:location?", searchRestaurants);
     app.get("/api/match?", matchRestaurant);
 
