2716ec1e2895c1897f3941b22d5a1f5feb794feb
Qiansha Meng <mengqianshasha@163.com>
2021-12-12 11:42:39 -0800

    deploy server

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const axios = require('axios');
 
 /************************************ Configure CORS************************************/
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", CONSTANTS.CLIENT_URL);
+    // res.header("Access-Control-Allow-Origin", CONSTANTS.CLIENT_URL);
+    res.header("Access-Control-Allow-Origin", req.header('Origin'));
     res.header("Access-Control-Allow-Headers",
                "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
