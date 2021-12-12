02b787ee62956e7e8d6b2bcbd69e640e40125439
ZihanWangRepo <wangzihan1989@gmail.com>
2021-12-12 14:36:54 -0800

    Update activity and notification backend

--- services/review-service.js
+++ services/review-service.js
@@ -1,5 +1,7 @@
 const reviewDao = require('../data/db/review/review-dao');
-const userDao = require('../data/db/user/user-dao')
+const userDao = require('../data/db/user/user-dao');
+const activityDao = require('../data/db/activity/activity-dao');
+const notificationDao = require('../data/db/notification/notification-dao')
 const moment = require('moment')
 
 module.exports = (app) => {
@@ -15,15 +17,60 @@ module.exports = (app) => {
     });
   }
 
+  const updateProfileReviews = async (review) => {
+    let user = {}
+    const findUser = userDao.findUserById(review['user'])
+    .then(res => {
+      user = res;
+    })
+    await findUser;
+    user.customerData.reviews.push(review._id.toString())
+    return user
+  }
+
   const postNewReview = (req, res) => {
     const newReview = req.body;
-    return reviewDao.createReview(newReview).then(insertedReview => res.send(insertedReview))
+    return reviewDao.createReview(newReview).then(insertedReview =>
+    { updateProfileReviews(insertedReview).then(user => {
+      req.session['profile'] = user;
+      userDao.updateUser(user).then(status=> console.log("user updated"))});
+      const activity = {
+        user: insertedReview.user,
+        type: "review",
+        time_created: insertedReview.time_created,
+        review: insertedReview._id.toString()
+      }
+      activityDao.createActivity(activity);
+      const notification = {
+        user: insertedReview.user,
+        type: "new-review",
+        time_created: insertedReview.time_created,
+        review: insertedReview._id.toString()
+      };
+      notificationDao.createNotification(notification);
+      res.send(insertedReview)
+    })
+  }
 
+  const deleteProfileReview = async (review) => {
+    let user = {}
+    const findUser = userDao.findUserById(review['user'])
+    .then(res => {
+      user = res;
+    })
+    await findUser;
+    user.customerData.reviews = user.customerData.reviews.filter(id => id !== review._id.toString())
+    return user
   }
 
   const deleteReview = (req, res) => {
     const reviewId = req.params.reviewId;
     reviewDao.deleteReview(reviewId).then(status => res.send(status));
+    reviewDao.findReviewById(reviewId).then(review => deleteProfileReview(review)
+        .then(user => {
+          req.session['profile'] = user;
+          userDao.updateUser(user)
+        }))
   }
 
   const saveReview = (req, res) => {
@@ -31,7 +78,15 @@ module.exports = (app) => {
   }
 
   const updateReply = (req, res) => {
-    reviewDao.updateReview(req.params.reviewId, req.body).then(status => res.send(status))
+    const newReview = req.body;
+    reviewDao.updateReview(req.params.reviewId, req.body).then(status => res.send(status));
+    const activity = {
+      user: newReview.replies[0].user,
+      type: "reply-review",
+      time_created: newReview.time_created,
+      review: newReview._id.toString()
+    }
+    activityDao.createActivity(activity);
   }
 
   const findAllReviewsByIdsAsync = async (reviewsId) => {
