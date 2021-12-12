d26ea7ecff6c5304be542c7368e6af6fb29871a5
Qiansha Meng <mengqianshasha@163.com>
2021-12-12 12:54:11 -0800

    server deploy

--- CONSTS.js
+++ CONSTS.js
@@ -1,5 +1,5 @@
 // const CLIENT_URL = "http://localhost:3000";
-const CLIENT_URL = "https://thirsty-brown-7f2102.netlify.app/";
+const CLIENT_URL = "https://thirsty-brown-7f2102.netlify.app";
 const MONGODB_URL = 'mongodb+srv://fast-foodie:fast-foodie@cluster0.07hvq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 // const MONGODB_URL = 'mongodb://localhost:27017/foodie';
 
