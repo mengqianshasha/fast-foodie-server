f30c482d59efef29eeb5849fd0fd4fd2fef2677a
Qiansha Meng <mengqianshasha@163.com>
2021-12-13 22:37:26 -0800

    deploy server

--- CONSTS.js
+++ CONSTS.js
@@ -1,10 +1,11 @@
 
-const CLIENT_URL = "http://localhost:3000";
-// const CLIENT_URL = "https://thirsty-brown-7f2102.netlify.app";
-// const MONGODB_URL = 'mongodb+srv://fast-foodie:fast-foodie@cluster0.07hvq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const MONGODB_URL = 'mongodb://localhost:27017/foodie';
+// const CLIENT_URL = "http://localhost:3000";
+const CLIENT_URL = "https://thirsty-brown-7f2102.netlify.app";
+const MONGODB_URL = 'mongodb+srv://fast-foodie:fast-foodie@cluster0.07hvq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const MONGODB_URL = 'mongodb://localhost:27017/foodie';
 
-const ADMIN_ID_FANGYING = "61b369cdce57907b9a43eb34"
+// const ADMIN_ID_FANGYING = "61b369cdce57907b9a43eb34";
+const ADMIN_ID_FANGYING = "61b27099141351ab03bf256a";
 
 module.exports = {
     CLIENT_URL, MONGODB_URL, ADMIN_ID_FANGYING
